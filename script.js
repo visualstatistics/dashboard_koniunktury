cedf9700465b334cde6568fc97802e791dbfe8ed
visualstatistics <kamilgrabarczykuz@gmail.com>
2025-04-06 08:52:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const ctx = document.getElementById('myChart').getContext('2d');
 new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień'],
-        datasets: [{
-            label: 'Przykładowe dane',
-            data: [12, 19, 3, 5],
-            backgroundColor: 'rgba(75, 192, 192, 0.6)',
-        }]
-    }
+  type: 'bar',
+  data: {
+    labels: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień'],
+    datasets: [{
+      label: 'Przykładowe dane',
+      data: [12, 19, 3, 5],
+      backgroundColor: 'rgba(75, 192, 192, 0.6)',
+    }]
+  }
 });
